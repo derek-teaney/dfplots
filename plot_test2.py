be7a4d8b515127d60ae61f5fe008e9de479f62fa
derek-teaney <derek.teaney@stonybrook.edu>
2024-11-14 10:13:23 -0500

    updates

--- plot_test2.py
+++ plot_test2.py
@@ -6,7 +6,6 @@ import json
 import scienceplots
 import matplotlib.colors as mcolors
 
-
 # Set parameters to make it look like gnuplot
 plt.style.use(['science', 'nature'])
 
@@ -25,7 +24,7 @@ plt.rcParams['ytick.labelsize'] = 8
 
 
 ################################################################################
-def getEKTdata(lambdaekt,fourpietabys, tag='Ttt', testcase='test1'):
+def getEKTdata(lambdaekt, testcase='test1', tag='Ttt'):
     """ Returns the EKT data. The output is 
 
     x[:], TttEKT[:], TttDF[:], finaltime 
@@ -46,47 +45,40 @@ def getEKTdata(lambdaekt,fourpietabys, tag='Ttt', testcase='test1'):
 
     # Get Either the  Ttt component or Ttx component
     Tttekt = nug*EKTdata[sl,:,variables[tag]]
-    return xekt, Tttekt
-
-def getEKTdata(lambdaekt,fourpietabys, tag='Ttt'):
-    """ Returns the EKT data. The output is 
-
-    x[:], TttEKT[:]
-    """
-
-    Nz=150
-    nug=16
-
-    EKTdata=np.loadtxt("./new_EKT/test1_L%d_gluon_Tmunu_vs_time.out" % lambdaekt)
-    nx,ny = EKTdata.shape
-    EKTdata.shape=(nx//Nz,Nz,ny)
-    sl=-1
-    xekt = EKTdata[0,:,18] 
-    Tttekt = nug*EKTdata[sl,:,8]
 
     return xekt, Tttekt
 
 
 ################################################################################
+
+# this returns the final names for the kinetic theory tests and is a helper for getdfdata
 def getnames(fourpietabys, gaussian_const, gaussian_amplitude, gaussian_width):
-    """ Return the filename for a gvien value of the paramaeters. This a helper function used by getDFdata """
     s='DFAndBDNK/nbys_{}_d_{}_A_{}_w_{}'.format(fourpietabys, gaussian_const, gaussian_amplitude, gaussian_width)
     return './' + s.replace('.', 'd')
 
-def getDFdata(fourpietabys,gaussian_const, gaussian_amplitude, gaussian_width, tag='Ttt'):
+
+# Structure holding the parameters for test1 and test2 which will be compared to kinetic theory. This is used  by getDFdata
+testcases = { 'test1' : 
+                 {'const': 0.12, 'amplitude': 0.48  , 'width': 25.0, 'finaltime': 47.5, }, 
+              'test2'  :
+                 {'const': 0.06 , 'amplitude': 9.6 , 'width':  25.0, 'finaltime': 47.5, }, 
+             }
+
+def getDFdata(fourpietabys,testcase='test1', tag='Ttt'):
 
     """  Returns the density frame and BDNK data for a given run 
-
 
     The output is x[:], TttDF[:], TttBDNK[:], TttIdeal[:], info
 
 
-    The info is an associative array containing the parameters of the run.
+    The info is an associative array containing the parameters of the run. If tag  can be 'Ttt' , 'Ttx', 'eps', or 'ux'
     """
 
-    variables = {'Ttt':0, 'Ttx':1, 'eps':2, 'ux':3 } 
-    name = getnames(fourpietabys, gaussian_const, gaussian_amplitude, gaussian_width)
-    name0 = getnames(0.0, gaussian_const, gaussian_amplitude, gaussian_width)
+    variables = {'Ttt':0, 'Ttx':1, 'eps':2, 'ux':3 }  
+    params = testcases[testcase]
+
+    name = getnames(fourpietabys, params['const'], params['amplitude'] , params['width'])
+    name0 = getnames(0.0, params['const'], params['amplitude'], params['width'])
 
     print(name, name0)
     data = json.load(open(name + '.json'))
@@ -112,80 +104,76 @@ def getDFdata(fourpietabys,gaussian_const, gaussian_amplitude, gaussian_width, t
 
 
 ################################################################################
-def freefunction(finaltime, gaussian_const = 0.12, gaussian_amplitude = 0.48, gaussian_width = 25.0):
+def freefunction(testcase='test1'):
     """ Returns for the free theory 
 
         x[:], Ttt[:], Ttx[:]
 
     """
-    x=np.linspace(-75.1,75.1,500)
-    delta=gaussian_const
-    A=gaussian_amplitude
-    w=np.sqrt(gaussian_width)
 
-    tc=finaltime
+    x=np.linspace(-75.1,75.1,500)
+    delta=testcases[testcase]['const']
+    A=testcases[testcase]['amplitude'] 
+    w=np.sqrt(testcases[testcase]['width'])
+    tc=testcases[testcase]['finaltime']
+
     analytic=delta+np.sqrt(np.pi)/2*A*w/(2*tc)*(sp.erf((x+tc)/w)-sp.erf((x-tc)/w))
     analyticflux= A*w/(4*tc**2)*( w*( np.exp(-(tc+x)**2/w**2  ) - np.exp(-(tc-x)**2/w**2) ) +   np.sqrt(np.pi)*x*(sp.erf((x+tc)/w)-sp.erf((x-tc)/w))  )
 
     return  x, analytic, analyticflux
 
 
-########################################################################
-def plotIC(case='DF', lambda_case=0):
-    """Plots the initial conditions """ 
+#########################################################################
+#def plotIC(case='DF', lambda_case=0):
+#    """Plots the initial conditions """ 
 
-    listetas=[0.180, 0.513, 1.48]
-    listlambda=[20, 10, 5]
+#    listetas=[0.180, 0.513, 1.48]
+#    listlambda=[20, 10, 5]
 
-    etas4pi=4*np.pi*np.array(listetas)
+#    etas4pi=4*np.pi*np.array(listetas)
 
-    gaussian_const = 0.12
-    gaussian_amplitude = 0.48
-    gaussian_width = 25.0
+#    gaussian_const = 0.12
+#    gaussian_amplitude = 0.48
+#    gaussian_width = 25.0
 
-    fig1, ax1 = plt.subplots()
-    ax1.set_xlim(-15, 15)
-    #ax1.set_xticks([-75,-50,-25,0,25,50,75])
-    ax1.set_ylim(-0.1, 0.65)
+#    fig1, ax1 = plt.subplots()
+#    ax1.set_xlim(-15, 15)
+#    #ax1.set_xticks([-75,-50,-25,0,25,50,75])
+#    ax1.set_ylim(-0.1, 0.65)
 
-    i = lambda_case
+#    i = lambda_case
 
-    xfree0, freeTtt0, freeTtx0 = freefunction(0.000001)
+#    xfree0, freeTtt0, freeTtx0 = freefunction(0.000001)
 
 
-    if case == 'DF':
-        ax1.plot(xfree0, freeTtt0, "C0", linewidth=1.2, label='Density Frame') 
-    else:
-        ax1.plot(xfree0, freeTtt0,  "C0", linewidth=1.2, label='BDNK') 
+#    if case == 'DF':
+#        ax1.plot(xfree0, freeTtt0, "C0", linewidth=1.2, label='Density Frame') 
+#    else:
+#        ax1.plot(xfree0, freeTtt0,  "C0", linewidth=1.2, label='BDNK') 
 
-    ax1.plot(xfree0, freeTtt0, "C3", linestyle=(0,(3,2)), linewidth=1.2, label='QCD kinetics')
+#    ax1.plot(xfree0, freeTtt0, "C3", linestyle=(0,(3,2)), linewidth=1.2, label='QCD kinetics')
 
-    # ax1.plot(x, ideal, "k--", linewidth=0.5, label=r"$\eta/s=0$ and $\infty$") 
-    # ax1.plot(xfree, freeTtt, "k--", linewidth=0.5)
+#    # ax1.plot(x, ideal, "k--", linewidth=0.5, label=r"$\eta/s=0$ and $\infty$") 
+#    # ax1.plot(xfree, freeTtt, "k--", linewidth=0.5)
 
-    ax1.legend(frameon=True,loc="lower left", fancybox=False, framealpha=0.8, edgecolor='white')
-    # # Set a text label with the value of eta/s
-    ax1.annotate('initial conditions', (0.85,0.25), xycoords='figure fraction', ha='right',bbox=dict(alpha=0.8,facecolor='white',edgecolor='white'))
+#    ax1.legend(frameon=True,loc="lower left", fancybox=False, framealpha=0.8, edgecolor='white')
+#    # # Set a text label with the value of eta/s
+#    ax1.annotate('initial conditions', (0.85,0.25), xycoords='figure fraction', ha='right',bbox=dict(alpha=0.8,facecolor='white',edgecolor='white'))
 
-    ax1.annotate(r'$A=0.48\;{\rm GeV}$'+"\n" + r"$\delta=0.12\; {\rm GeV}$" + "\n" + r"$L=5 \;{\rm GeV}^{-1}$", xy=(0.7,0.7), xycoords='figure fraction', linespacing=1.5)
+#    ax1.annotate(r'$A=0.48\;{\rm GeV}$'+"\n" + r"$\delta=0.12\; {\rm GeV}$" + "\n" + r"$L=5 \;{\rm GeV}^{-1}$", xy=(0.7,0.7), xycoords='figure fraction', linespacing=1.5)
 
-    ax1.set_xlabel(r'$x$')
-    ax1.set_ylabel(r'$T^{tt}$')
-    fig1.tight_layout() 
+#    ax1.set_xlabel(r'$x$')
+#    ax1.set_ylabel(r'$T^{tt}$')
+#    fig1.tight_layout() 
 
-    #name = "{}_{}.pdf".format(case,listlambda[lambda_case])
-    fig1.savefig("test1_ic.pdf")
-    #fig1.savefig(name)
+#    #name = "{}_{}.pdf".format(case,listlambda[lambda_case])
+#    fig1.savefig("test1_ic.pdf")
+#    #fig1.savefig(name)
 
 
 ########################################################################
 def plotStress(case='DF', lambda_case=0):
     """Makes a nice comparison between the density frame and kintec theory or BDNK and kinetic theory """
-
-    finaltime=47.5
-    gaussian_const = 0.12
-    gaussian_amplitude = 0.48
-    gaussian_width = 25.0
 
     listetas=[0.180, 0.513, 1.48]
     etas4pi=4*np.pi*np.array(listetas)
@@ -200,13 +188,13 @@ def plotStress(case='DF', lambda_case=0):
 
     # Get the kinetic theory data
     et = listetas[i] 
-    xekt, Tttekt = getEKTdata(listlambda[i], et)
+    xekt, Tttekt  = getEKTdata(listlambda[i])
 
     # Get density frame, bdnk, and idealhydro
-    x, df, bdnk, ideal, data = getDFdata(etas4pi[i], gaussian_const, gaussian_amplitude, gaussian_width)
+    x, df, bdnk, ideal, data = getDFdata(etas4pi[i])
 
     # Get Free streaming data
-    xfree, freeTtt, freeTtx = freefunction(finaltime)
+    xfree, freeTtt, freeTtx = freefunction()
 
 
     if case == 'DF':
@@ -254,13 +242,13 @@ def plotKTPlot1(case='DF'):
 
         # Get the kinetic theory data
         et = listetas[i] 
-        xekt, Tttekt = getEKTdata(listlambda[i], et)
+        xekt, Tttekt  = getEKTdata(listlambda[i])
 
         # Get density frame, bdnk, and idealhydro
-        x, df, bdnk, ideal, data = getDFdata(etas4pi[i], gaussian_const, gaussian_amplitude, gaussian_width)
+        x, df, bdnk, ideal, data = getDFdata(etas4pi[i])
 
         # Get Free streaming data
-        xfree, freeTtt, freeTtx = freefunction(finaltime)
+        xfree, freeTtt, freeTtx = freefunction()
 
 
         # For the first plot add the label
@@ -294,10 +282,7 @@ def plotKTPlot1(case='DF'):
 def plotKTPlot1b(case='DF'):
     """Makes a nice comparison between the density frame and kintec theory for moderate coupling """
 
-    finaltime = 47.5
-    gaussian_const = 0.12
-    gaussian_amplitude = 0.48
-    gaussian_width = 25.0
+    finaltime=47.5
 
     listetas=[0.180, 0.513, 1.48]
     etas4pi=4*np.pi*np.array(listetas)
@@ -312,13 +297,13 @@ def plotKTPlot1b(case='DF'):
     i= 2
     # Get the kinetic theory data
     et = listetas[i] 
-    xekt, Tttekt = getEKTdata(listlambda[i], et)
+    xekt, Tttekt  = getEKTdata(listlambda[i])
 
     # Get density frame, bdnk, and idealhydro
-    x, df, bdnk, ideal, data = getDFdata(etas4pi[i], gaussian_const, gaussian_amplitude, gaussian_width)
+    x, df, bdnk, ideal, data = getDFdata(etas4pi[i])
 
     # Get Free streaming data
-    xfree, freeTtt, freeTtx = freefunction(finaltime)
+    xfree, freeTtt, freeTtx = freefunction()
 
 
     if case == 'DF':
@@ -339,13 +324,130 @@ def plotKTPlot1b(case='DF'):
 
     fig1.savefig('KTPlot1b.pdf')
 
+def plotKTPlot2(case='DF'):
+    """Makes a nice comparison between the density frame and kintec theory for moderate coupling """
+
+    finaltime=47.5
+    gaussian_const = 0.06
+    gaussian_amplitude = 9.6
+    gaussian_width = 25.0
+
+    listetas=[0.180, 0.513, 1.48]
+    etas4pi=4*np.pi*np.array(listetas)
+    listlambda=[20, 10, 5]
+
+    fig1, ax1 = plt.subplots()
+    ax1.set_xlim(-75, 75)
+    ax1.set_xticks([-75,-50,-25,0,25,50,75])
+    ax1.set_ylim(0.0, 4.5)
+
+    colors = ['C3',mcolors.CSS4_COLORS['peachpuff'],'C2']
+    #colors = ['C3',mcolors.TABLEAU_COLORS['tab:pink'],'C2']
+    #colors = ['C3',mcolors.BASE_COLORS['m'],'C2']
+
+    print(mcolors.BASE_COLORS)
+    print(mcolors.TABLEAU_COLORS)
+
+    labels=[]
+    for i  in range(0,3):
+        labels.append(r'$4\pi\eta/s={:.1f}$'.format(4.0*np.pi*listetas[i]))
+    print(labels)
+
+    for i in range(0,3):
+
+        # Get the kinetic theory data
+        et = listetas[i] 
+        xekt, Tttekt  = getEKTdata(listlambda[i], testcase='test2')
+
+        # Get density frame, bdnk, and idealhydro
+        x, df, bdnk, ideal, data = getDFdata(etas4pi[i], testcase='test2')
+
+        # Get Free streaming data
+        xfree, freeTtt, freeTtx = freefunction(testcase='test2')
+
+        ax1.plot(xekt, Tttekt,'C0', linewidth=1.2) 
+
+        # if i == 0:
+        #     ax1.plot(x, ideal, "k--", linewidth=0.5, label=r"ideal hydro") 
+
+        ax1.plot(xekt, Tttekt, color=colors[i], linewidth=1.2, linestyle="--", label=labels[i])
+
+    #ax1.plot(xfree, freeTtt, "k:", linewidth=0.5, label=r"free stream") 
+    ax1.set_xlabel(r'$x$')
+    ax1.set_ylabel(r'$T^{tt}$')
+    ax1.legend(loc="upper left",title='QCD kinetics', fontsize=8)
+
+    ax1.annotate("test 2", (0.87,0.8), ha='right', xycoords='figure fraction')
+    fig1.savefig('KineticOnlyT2.pdf')
+
+def plotKTPlot1(case='DF'):
+    """ Plots kinetic theory results for test1 """
+
+    finaltime = 47.5
+    gaussian_const = 0.12
+    gaussian_amplitude = 0.48
+    gaussian_width = 25.0
+
+    listetas=[0.180, 0.513, 1.48]
+    etas4pi=4*np.pi*np.array(listetas)
+    listlambda=[20, 10, 5]
+
+    fig1, ax1 = plt.subplots()
+    ax1.set_xlim(-75, 75)
+    ax1.set_ylim(0.0, 0.5)
+    ax1.set_xticks([-75,-50,-25,0,25,50,75])
+    #ax1.set_ylim(0.0, 3.25)
+
+    colors = ['C3',mcolors.CSS4_COLORS['peachpuff'],'C2']
+    #colors = ['C3',mcolors.TABLEAU_COLORS['tab:pink'],'C2']
+    #colors = ['C3',mcolors.BASE_COLORS['m'],'C2']
+
+    print(mcolors.BASE_COLORS)
+    print(mcolors.TABLEAU_COLORS)
+
+    labels=[]
+    for i  in range(0,3):
+        labels.append(r'$4\pi\eta/s={:.1f}$'.format(4.0*np.pi*listetas[i]))
+    print(labels)
+
+    for i in range(0,3):
+
+        # Get the kinetic theory data
+        et = listetas[i] 
+        xekt, Tttekt = getEKTdata(listlambda[i], testcase='test1')
+
+        # Get density frame, bdnk, and idealhydro
+        x, df, bdnk, ideal, data = getDFdata(etas4pi[i], testcase='test1')
+
+        # Get Free streaming data
+        xfree, freeTtt, freeTtx = freefunction()
+
+        ax1.plot(xekt, Tttekt,'C0', linewidth=1.2) 
+
+        #if i == 0:
+            #ax1.plot(x, ideal, "k--", linewidth=0.5, label=r"ideal hydro") 
+
+        ax1.plot(xekt, Tttekt, color=colors[i], linewidth=1.2, linestyle="--", label=labels[i])
+
+    #ax1.plot(xfree, freeTtt, "k:", linewidth=0.5, label=r"free stream") 
+    ax1.set_xlabel(r'$x$')
+    ax1.set_ylabel(r'$T^{tt}$')
+    ax1.legend(loc="upper center",fontsize=8)
+
+    ax1.annotate("QCD kinetics\ntest 1", (0.87,0.8), ha='right', xycoords='figure fraction')
+    fig1.savefig('KineticOnlyT1.pdf')
+
+plotKTPlot2(case='DF')
+
+
+
 # plotIC()
-# plotStress(lambda_case=0)
-# plotStress(lambda_case=1)
-# plotStress(lambda_case=2)
-# plotStress(case='BDNK', lambda_case=0)
-# plotStress(case='BDNK', lambda_case=1)
-# plotStress(case='BDNK', lambda_case=2)
+plotStress(lambda_case=0)
+plotStress(lambda_case=1)
+plotStress(lambda_case=2)
+plotStress(case='BDNK', lambda_case=0)
+plotStress(case='BDNK', lambda_case=1)
+plotStress(case='BDNK', lambda_case=2)
 
 plotKTPlot1()
 plotKTPlot1b()
